2b98d3d9f1782c6689ebec044727922fb62cbcbf
Harry James Hall <mission.deny.the.mission@gmail.com>
2024-06-17 20:46:17 +0100

    Change vscode to vscodium fixing packaging issues

--- build.sh
+++ build.sh
@@ -12,14 +12,22 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-curl -Lo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
+# install vscodium
+rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+rpm-ostree install codium
 
+# install docker from the offical repo
+curl -Lo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
 rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# this would install a package from rpmfusion
+# install vim from rpmfusion
 rpm-ostree install vlc
 
+# Install virtual machine software
+rpm-ostree install virt-manager edk2-ovmf qemu
 
-#### Example for enabling a System Unit File
+# Enable docker and libvird
 
 systemctl enable docker
+systemctl enable libvirtd
