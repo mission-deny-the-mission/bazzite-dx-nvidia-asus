d3e091b2b9d51c80e9f5af04f4e10594441a98a4
Harry James Hall <mission.deny.the.mission@gmail.com>
2024-06-17 20:11:06 +0100

    Make some modifications, and add public key for signing

--- build.sh
+++ build.sh
@@ -12,12 +12,14 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+curl -Lo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # this would install a package from rpmfusion
-# rpm-ostree install vlc
+rpm-ostree install vlc
+
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+systemctl enable docker
